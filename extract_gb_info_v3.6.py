9ff37e76c938604fd6c18f4997803e990a7df3d0
Finley Grover-Thomas <finleythomas6@gmail.com>
2023-04-12 14:58:23 +0100

    Update to include columns with notes and cell line data

--- extract_gb_info_v3.6.py
+++ extract_gb_info_v3.6.py
@@ -6,14 +6,16 @@ if len(sys.argv) != 2:
     sys.exit()
 
 input_file = sys.argv[1]
-output_file = "genbank_output.tsv"
+output_file = "genbank_output_D.tsv"
 
 with open(input_file, "r") as f:
     with open(output_file, "w") as out:
-        out.write("Accession\tOrganism\tCountry\tHaplogroup\tSequence\n")
+        out.write("Accession\tOrganism\tCountry\tNote\tCell_Line\tHaplogroup\tSequence\n")
         accession = ""
         organism = ""
         country = ""
+        note = ""
+        cell_line = ""
         haplogroup = ""
         sequence = ""
         in_features = False
@@ -26,6 +28,10 @@ with open(input_file, "r") as f:
                 organism = line.split("  ")[-1].strip()
             elif line.startswith("  /country") or line.startswith("country"):
                 country = line.split("=")[-1].strip().strip('"')
+            elif line.startswith("  /note") or line.startswith("note"):
+                note = line.split("=")[-1].strip().strip('"')
+            elif line.startswith("  /cell_line") or line.startswith("/cell_line"):
+                cell_line = line.split("=")[-1].strip().strip('"')
             elif line.startswith("SOURCE"):
                 in_source = True
             elif line.startswith("FEATURES"):
@@ -38,20 +44,26 @@ with open(input_file, "r") as f:
                     sequence_lines.append(next_line.strip())
                 sequence = ''.join(sequence_lines)
                 sequence = re.sub(r'\d', '', sequence)
-                out.write(accession + "\t" + organism + "\t" + country + "\t" + haplogroup + "\t" + sequence + "\n")
+                out.write(accession + "\t" + organism + "\t" + country + "\t" + note + "\t" + cell_line + "\t" + haplogroup + "\t" + sequence + "\n")
                 accession = ""
                 organism = ""
                 country = ""
+                note = ""
+                cell_line = ""
                 haplogroup = ""
                 sequence = ""
                 in_features = False
             elif in_features and "/country=" in line:
                 country = line.split("=")[-1].strip().strip('"')
+            elif in_features and "/note=" in line and not note:
+                note = line.split("=")[-1].strip().strip('"')
+            elif in_features and "/cell_line=" in line:
+                cell_line = line.split("=")[-1].strip().strip('"')
             elif in_source and "/haplogroup=" in line:
                 haplogroup = line.split("=")[-1].strip().strip('"')
             elif line.startswith("//"):
                 in_source = False
         if accession:
-            out.write(accession + "\t" + organism + "\t" + country + "\t" + haplogroup + "\t" + sequence + "\n")
+            out.write(accession + "\t" + organism + "\t" + country + "\t" + note + "\t" + cell_line + "\t" + haplogroup + "\t" + sequence + "\n")
 
 print("Extraction complete. Results saved to " + output_file + ".")
